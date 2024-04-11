3e2f5e4e11ed5796fabca53d2c84d8824ebd8581
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-11 14:03:21 -0500

    Moved and corrected address tests.

--- test/FunctionSafer.Tests/Core/AddressTests.cs
+++ test/FunctionSafer.Tests/Core/AddressTests.cs
@@ -1,14 +1,14 @@
 using FunctionSafer.Core;
 
-namespace FunctionSafer.Tests;
+namespace FunctionSafer.Tests.Core;
 
 public class AddressTests
 {
     [Fact]
     public void Address_Addition()
     {
-        var address1 = new Address(0x00000000);
-        var address2 = new Address(0x00000001);
+        var address1 = new Address(0x0);
+        var address2 = new Address(0x01);
         var result = address1 + address2;
         Assert.Equal(0x00000001u, result.Value);
     }
@@ -16,8 +16,8 @@ public class AddressTests
     [Fact]
     public void Address_Subtraction()
     {
-        var address1 = new Address(0x00000001);
-        var address2 = new Address(0x00000000);
+        var address1 = new Address(0x1);
+        var address2 = new Address(0x0);
         var result = address1 - address2;
         Assert.Equal(0x00000001u, result.Value);
     }
@@ -25,7 +25,7 @@ public class AddressTests
     [Fact]
     public void Address_ToString()
     {
-        var address = new Address(0x00000000);
+        var address = new Address(0x0);
         Assert.Equal("00000000", address.ToString());
     }
 }
