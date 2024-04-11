e4c9fa8ef3363b46ebd9a709a57a406021833bcf
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-11 15:37:41 -0500

    Defined function model.

---
+++ test/FunctionSafer.Tests/Core/Extensions/Function/GetSizeTests.cs
@@ -0,0 +1,22 @@
+using FunctionSafer.Core;
+
+namespace FunctionSafer.Tests.Core;
+
+public partial class FunctionTests
+{
+    [Fact]
+    public void Function_GetSize_ReturnsSize()
+    {
+        var functionAddress = new Address(0x0020_0000u);
+        var functionHook = new Instruction(functionAddress, 0x1u);
+        var function = new Function("test function", functionHook);
+        var functionSize = function.GetSize();
+        Assert.Equal(4u, functionSize);
+        
+        var instructionAddress = new Address(0x4u);
+        var instruction = new Instruction(instructionAddress, 0x0u);
+        function.AddInstruction(instruction);
+        var expandedFunctionSize = function.GetSize();
+        Assert.Equal(8u, expandedFunctionSize);
+    }
+}
